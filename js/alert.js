42172278a305272328b39cf309809065d0d926c6
Richard Hawk <rrhawk3@gmail.com>
2022-03-21 20:05:35 -0400

    Add variables to alert.js

--- js/alert.js
+++ js/alert.js
@@ -1,1 +1,7 @@
-alert( "I'm JavaScript!" );
+"use strict"; //NOTE: Investigate this further. Most tutorials encourage "use strict".
+
+//This is a sandbox file for testing JavaScript learnings
+//Finished on https://javascript.info/variables
+let message = "I'm Javascript!", user = "John", age = "25";
+alert( message );
+alert( "I'm Javascript!" );
